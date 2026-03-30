76c7df2c9d77dd1d90348490a7c984d835310326
feytyz-vlad <237119@kisit.kneu.edu.ua>
2026-03-30 13:22:43 +0300

    Add files via upload

---
+++ ci-cd-lr-project-main/test/simple.test.ts
@@ -0,0 +1,5 @@
+import { test, expect } from 'vitest'
+
+test('перевірка що vitest працює', () => {
+  expect(1 + 1).toBe(2)
+})
---
+++ ci-cd-lr-project-main/vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/ci-cd-lr-project/',
+  build: {
+    rollupOptions: {
+      output: {
+        dir: 'dist',
+        entryFileNames: 'ci-cd-lr-project.js'
+      }
+    }
+  },
+  plugins: [react()],
+})
